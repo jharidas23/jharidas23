5758c0136c543d0e7fb02776994be2dcfbeb43d2
gracegilbert99 <105007135+gracegilbert99@users.noreply.github.com>
2022-06-02 12:46:47 -0400

    Update CookedPatty.java

--- CookedPatty.java
+++ CookedPatty.java
@@ -1,8 +1,14 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 import javax.swing.JComponent;
 
@@ -12,7 +18,12 @@ public class CookedPatty extends JComponent{
 	private Ellipse2D.Double bottom;
 	private Rectangle middle;
 	  
-	public CookedPatty(int x, int y){
+	private boolean dropped = false;
+	private boolean donePlacing = false;
+	
+	private CookedPatty obj = this;
+	
+	public CookedPatty(int x, int y, ArrayList<JComponent> stackedItems, char screen){
 		
 		setLocation(x,y);
 		setSize(101, 61);
@@ -21,7 +32,75 @@ public class CookedPatty extends JComponent{
 		middle = new Rectangle(0,10,100,15);
 		top = new Ellipse2D.Double(0,0,100,20);
 		
+		addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if(!dropped && screen == 'a') {
+					Point p = MouseInfo.getPointerInfo().getLocation();
+					int mouseX = p.x;
+					int mouseY = p.y;
+					
+					setLocation(mouseX-100,mouseY-75);
+				}
+				if(screen == 'c') {
+					Point p = MouseInfo.getPointerInfo().getLocation();
+					int mouseX = p.x;
+					int mouseY = p.y;
+					
+					setLocation(mouseX-100,mouseY-75);
+				}
+				
+				
+				
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		
+		addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+
+				if(!donePlacing && screen == 'a') {
+					dropped = true;
+					stackedItems.add(obj);
+					donePlacing = true;
+				}				
+			
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -36,6 +115,10 @@ public class CookedPatty extends JComponent{
 		g2.setColor(new Color(153,76,0));
 		g2.fill(top);
 		
+	}
+	
+	public String getName() {
+		return("CookedPatty");
 	}
 	
 }
