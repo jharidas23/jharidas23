c1824f9fb6626202d955dc4edc26868e0426473b
jharidas23 <62947064+jharidas23@users.noreply.github.com>
2022-06-03 12:42:01 -0400

    Update BurntPatty.java

--- BurntPatty.java
+++ BurntPatty.java
@@ -11,19 +11,33 @@ import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
+import javax.swing.Timer;
 
 public class BurntPatty extends JComponent{
 	
 	private Ellipse2D.Double top;
 	private Ellipse2D.Double bottom;
 	private Rectangle middle;
+	private Timer t;
+	private Cook panel; 
+	private ArrayList<String> order; 
+	private String type;  
+	private char screen; 
+	
+	private BurntPatty obj = this; 
 	
 	private boolean dropped = false;
 	private boolean donePlacing = false;
 	
-	private BurntPatty obj = this;
-	  
-	public BurntPatty(int x, int y, ArrayList<String> stackedItems, char screen, AssemblePanel aPanel){
+	public BurntPatty(int x, int y, ArrayList<String> playerOrderCombo, char sc, Cook cookPanel){
+		
+		type = "Burnt"; 
+		
+		panel = cookPanel;
+		
+		order = playerOrderCombo; 
+		
+		screen = sc; 
 		
 		setLocation(x,y);
 		setSize(101, 61);
@@ -31,37 +45,6 @@ public class BurntPatty extends JComponent{
 		bottom = new Ellipse2D.Double(0,15,100,15);
 		middle = new Rectangle(0,10,100,15);
 		top = new Ellipse2D.Double(0,0,100,20);
-		
-		addMouseMotionListener(new MouseMotionListener() {
-
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				if(!dropped && screen == 'a') {
-					Point p = MouseInfo.getPointerInfo().getLocation();
-					int mouseX = p.x;
-					int mouseY = p.y;
-					
-					setLocation(mouseX-100,mouseY-75);
-				}
-				if(screen == 'c') {
-					Point p = MouseInfo.getPointerInfo().getLocation();
-					int mouseX = p.x;
-					int mouseY = p.y;
-					
-					setLocation(mouseX-100,mouseY-75);
-				}
-				
-				
-				
-			}
-
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
 		
 		addMouseListener(new MouseListener() {
 
@@ -79,14 +62,29 @@ public class BurntPatty extends JComponent{
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
-
-				if(!donePlacing && screen == 'a') {
-					dropped = true;
-					stackedItems.add("Patty");
-					donePlacing = true;
-				}				
-			
+				// TODO Auto-generated method stub
+				System.out.println("mouse released");
+				if(screen == 'C')
+				{
+					if((getX()>1000 && getX()<1150) && (getY()>510 && getY()<585))
+					{
+						screen = 'A'; 
+						BurgeriaMain.getCompeltePatties().add(obj); 
+						panel.remove(obj);
+						System.out.println("item has been removed"); 
+						panel.revalidate(); 
+						panel.repaint(); 
+					}
+				}
+				if(screen == 'A')
+				{
+					if(!donePlacing) {
+						dropped = true;
+						order.add("Patty");
+						donePlacing = true;
+				}
 			}
+		}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
@@ -96,6 +94,36 @@ public class BurntPatty extends JComponent{
 
 			@Override
 			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		}); 
+		
+		
+		addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if(!dropped && screen == 'A') {
+					Point p = MouseInfo.getPointerInfo().getLocation();
+					int mouseX = p.x;
+					int mouseY = p.y;
+					
+					setLocation(mouseX-100,mouseY-75);
+				}
+				if(screen == 'C') {
+					Point p = MouseInfo.getPointerInfo().getLocation();
+					int mouseX = p.x;
+					int mouseY = p.y;
+					
+					setLocation(mouseX-100,mouseY-75);
+				}
+				
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
 				// TODO Auto-generated method stub
 				
 			}
@@ -117,8 +145,9 @@ public class BurntPatty extends JComponent{
 		
 	}
 	
-	public String getName() {
-		return("BurntPatty");
+	public String getType()
+	{
+		return "Burnt"; 
 	}
 	
 }
