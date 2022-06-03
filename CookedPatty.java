3cb68579b16339b484e55c854eae78ccd8d927c9
gracegilbert99 <105007135+gracegilbert99@users.noreply.github.com>
2022-06-03 00:12:35 -0400

    Update CookedPatty.java

--- CookedPatty.java
+++ CookedPatty.java
@@ -23,7 +23,7 @@ public class CookedPatty extends JComponent{
 	
 	private CookedPatty obj = this;
 	
-	public CookedPatty(int x, int y, ArrayList<JComponent> stackedItems, char screen){
+	public CookedPatty(int x, int y, ArrayList<String> stackedItems, char screen, AssemblePanel aPanel){
 		
 		setLocation(x,y);
 		setSize(101, 61);
@@ -82,8 +82,9 @@ public class CookedPatty extends JComponent{
 
 				if(!donePlacing && screen == 'a') {
 					dropped = true;
-					stackedItems.add(obj);
+					stackedItems.add("Patty");
 					donePlacing = true;
+					
 				}				
 			
 			}
