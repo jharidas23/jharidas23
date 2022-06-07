19b466835bf26edc350d7f23c1a5a22a566eefe8
gracegilbert99 <105007135+gracegilbert99@users.noreply.github.com>
2022-06-07 10:33:19 -0400

    Update Onion.java

--- Onion.java
+++ Onion.java
@@ -3,6 +3,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -10,36 +11,31 @@ import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 
-public class Lettuce extends JComponent{
-
-	private Ellipse2D.Double bigOval;
-	private Ellipse2D.Double leaf1;
-	private Ellipse2D.Double leaf2;
-	private Ellipse2D.Double leaf3;
-	private Ellipse2D.Double leaf4;
-	private Ellipse2D.Double leaf5;
-	private Ellipse2D.Double leaf6;
-	private Ellipse2D.Double leaf7;
+public class Onion extends JComponent{
+	
+	private Ellipse2D.Double ellipse1;
+	private Ellipse2D.Double ellipse2;
+	private Ellipse2D.Double ellipse3;
+	private Ellipse2D.Double ellipse4;
+	private Ellipse2D.Double ellipse5;
 	
 	private boolean dropped = false;
 	private boolean donePlacing = false;
 	
-	private Lettuce obj = this;
-	
-	public Lettuce(int x, int y, ArrayList<String> stackedItems, ArrayList <JComponent> assembledObjs, AssemblePanel panel) {
+	private Onion obj = this;
+	  
+	public Onion(int x, int y, ArrayList<String> stackedItems, ArrayList <JComponent> assembledObjs, AssemblePanel panel){
+		
 		setLocation(x,y);
-		setSize(115, 60);
+		setSize(101, 61);
 		
-		bigOval = new Ellipse2D.Double(10,10,90,35);
-		leaf1 = new Ellipse2D.Double(15,30,30,20);
-		leaf2 = new Ellipse2D.Double(45,33,30,20);
-		leaf3 = new Ellipse2D.Double(76,19,10,30);
-		leaf4 = new Ellipse2D.Double(87,30,10,15);
-		leaf5 = new Ellipse2D.Double(80,10,30,20);
-		leaf6 = new Ellipse2D.Double(60,0,15,30);
-		leaf7 = new Ellipse2D.Double(15,5,15,30);
-		
+		ellipse1 = new Ellipse2D.Double(0,0,90,40);
+		ellipse2 = new Ellipse2D.Double(13,5,70,30);
+		ellipse3 = new Ellipse2D.Double(25,10,40,15);
+		ellipse4 = new Ellipse2D.Double(37,15,15,5);
+	
 		
 		addMouseMotionListener(new MouseMotionListener() {
 
@@ -50,8 +46,10 @@ public class Lettuce extends JComponent{
 					int mouseX = p.x;
 					int mouseY = p.y;
 					
-					setLocation(mouseX-100,mouseY-100);
+					setLocation(mouseX-80,mouseY-95);
 				}
+				
+				
 				
 			}
 
@@ -73,7 +71,6 @@ public class Lettuce extends JComponent{
 
 			@Override
 			public void mousePressed(MouseEvent e) {
-
 				panel.reorder(obj);
 				
 			}
@@ -83,13 +80,14 @@ public class Lettuce extends JComponent{
 
 				if(!donePlacing) {
 					dropped = true;
-					stackedItems.add("Lettuce");
+					stackedItems.add("Onion");
 					assembledObjs.add(obj);
 					donePlacing = true;
 					
 					//adjusting money
 					BurgeriaMain.changeMoney(-0.25);
 					panel.updateMoney();
+					
 				}				
 			
 			}
@@ -107,26 +105,32 @@ public class Lettuce extends JComponent{
 			}
 			
 		});
+		
+		
 	}
 	
 	public void paintComponent(Graphics g) {
-		
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(new Color(0,204,102));
-		g2.fill(bigOval);
-		g2.fill(leaf1);
-		g2.fill(leaf2);
-		g2.fill(leaf3);
-		g2.fill(leaf4);
-		g2.fill(leaf5);
-		g2.fill(leaf6);
-		g2.fill(leaf7);
 		
+		Color light = new Color(240,236,240);
+		Color dark = new Color(223, 220,223);
 		
+		g2.setColor(light);
+		g2.fill(ellipse1);
+		
+		g2.setColor(dark);
+		g2.fill(ellipse2);
+		
+		g2.setColor(light);
+		g2.fill(ellipse3);
+		
+		g2.setColor(dark);
+		g2.fill(ellipse4);
 		
 	}
 	
 	public String getName() {
-		return("Lettuce");
+		return("Onion");
 	}
+	
 }
